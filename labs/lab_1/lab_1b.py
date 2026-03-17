34dcf1493d6cda607fd3326830837f2a91711fdb
sinhatrisha <sinhat@bu.edu>
2026-03-17 19:31:46 -0400

    Update lab_1b.py
    Edits to fix if someone doesn't enter a float.

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -37,6 +37,16 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def request_sanitized_number(prompt: str) -> float:
+
+    while True:
+        try:
+            number = float(input(prompt))
+            return number
+        except ValueError:
+            print("Invalid input. Please print a valid number.")
+            
+    
 def main():
     
     print(f"===== Simple Calculator =====")
